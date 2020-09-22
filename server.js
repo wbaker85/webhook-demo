eeebb8a2539203b7f8390a7d06ce7a887c7ebe69
William Baker <wbaker@gmail.com>
2020-09-22 19:01:21 -0400

    asdf

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello Worldzzzzz!')
+});
 
 app.get('/webhook', (req, res) => {
   res.send('Got some stuff')
-})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
