00ac01daf0bfdc25bd17d7a8828c621063511611
William Baker <wbaker@gmail.com>
2020-09-22 18:54:56 -0400

    route

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/webhook', (req, res) => {
+  res.send('Got some stuff')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
